90c8848cf6b31ecd87c1c458c6cf89d487d28d9f
19981608 <cin.mishel@gmail.com>
2026-01-20 22:54:43 -0500

    Semana 7: Balanceo de Paréntesis y Torres de Hanoi en C#

---
+++ Balanceo_Parentesis/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+class BalanceoParentesis
+{
+    static void Main()
+    {
+        string expresion = "{7 + (8 * 5) - [(9 - 7) + (4 + 1)]}";
+
+        Console.WriteLine("Expresión evaluada:");
+        Console.WriteLine(expresion);
+
+        bool balanceada = VerificarBalanceo(expresion);
+
+        if (balanceada)
+        {
+            Console.WriteLine("Resultado: Fórmula balanceada.");
+        }
+        else
+        {
+            Console.WriteLine("Resultado: Fórmula NO balanceada.");
+        }
+    }
+
+    static bool VerificarBalanceo(string expresion)
+    {
+        Stack<char> pila = new Stack<char>();
+
+        foreach (char c in expresion)
+        {
+            if (c == '(' || c == '[' || c == '{')
+            {
+                pila.Push(c);
+            }
+            else if (c == ')' || c == ']' || c == '}')
+            {
+                if (pila.Count == 0)
+                    return false;
+
+                char ultimo = pila.Pop();
+
+                if (!Coinciden(ultimo, c))
+                    return false;
+            }
+        }
+
+        return pila.Count == 0;
+    }
+
+    static bool Coinciden(char apertura, char cierre)
+    {
+        return (apertura == '(' && cierre == ')') ||
+               (apertura == '[' && cierre == ']') ||
+               (apertura == '{' && cierre == '}');
+    }
+}
+
---
+++ TorresDeHanoi/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int totalDiscos = 3;
+
+        Stack<int> origen = new Stack<int>();
+        Stack<int> auxiliar = new Stack<int>();
+        Stack<int> destino = new Stack<int>();
+
+        for (int i = totalDiscos; i >= 1; i--)
+        {
+            origen.Push(i);
+        }
+
+        Console.WriteLine("Resolución de las Torres de Hanoi:\n");
+
+        ResolverHanoi(totalDiscos, origen, destino, auxiliar,
+                      "Origen", "Destino", "Auxiliar");
+
+        Console.WriteLine("\nProceso finalizado.");
+    }
+
+    static void ResolverHanoi(int n,
+                              Stack<int> origen,
+                              Stack<int> destino,
+                              Stack<int> auxiliar,
+                              string nombreOrigen,
+                              string nombreDestino,
+                              string nombreAuxiliar)
+    {
+        if (n == 1)
+        {
+            int disco = origen.Pop();
+            destino.Push(disco);
+            Console.WriteLine("Mover disco " + disco +
+                              " de " + nombreOrigen +
+                              " a " + nombreDestino);
+            return;
+        }
+
+        ResolverHanoi(n - 1, origen, auxiliar, destino,
+                      nombreOrigen, nombreAuxiliar, nombreDestino);
+
+        int discoActual = origen.Pop();
+        destino.Push(discoActual);
+        Console.WriteLine("Mover disco " + discoActual +
+                          " de " + nombreOrigen +
+                          " a " + nombreDestino);
+
+        ResolverHanoi(n - 1, auxiliar, destino, origen,
+                      nombreAuxiliar, nombreDestino, nombreOrigen);
+    }
+}
